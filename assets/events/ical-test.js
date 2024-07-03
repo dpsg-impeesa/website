d5a2dcbdad1796f063c442873326b290c59b17ae
Emhl <36165088+emhl@users.noreply.github.com>
2024-07-03 15:33:58 +0200

    Update ical-test.js

--- assets/events/ical-test.js
+++ assets/events/ical-test.js
@@ -2,8 +2,8 @@
   {
     events: {
       url: 'https://cloud.dpsg-bodensee.de/remote.php/dav/public-calendars/2TEopJmk2yQrBWQ9/?export',
-      format: 'ics'
+      format: 'ics',
+      color: "pink",     // an option!
+      textColor: "black" // an option!
     }
-    color: "pink",     // an option!
-    textColor: "black" // an option!
   }
