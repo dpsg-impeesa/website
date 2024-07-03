51f24dd04df6f50415cd468e17648b01d7e37125
Emhl <36165088+emhl@users.noreply.github.com>
2024-07-03 15:32:06 +0200

    test fullcalendar ical integration

---
+++ assets/events/ical-test.js
@@ -0,0 +1,9 @@
+  // ical feed Termine
+  {
+    events: {
+      url: 'https://cloud.dpsg-bodensee.de/remote.php/dav/public-calendars/2TEopJmk2yQrBWQ9/?export',
+      format: 'ics'
+    }
+    color: "pink",     // an option!
+    textColor: "black" // an option!
+  }
