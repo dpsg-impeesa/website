1a65147dea9c5adf74b9d3267e448e6b28c02ef2
emhl <emhl@steinr.eu>
2025-06-15 22:53:11 +0200

    wechse auf kalender von neuer nextcloud instanz

--- static/kalender/get-calendars.sh
+++ static/kalender/get-calendars.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-NC_URL="https://dpsg-radolfzell-cloud.de/remote.php/dav/public-calendars/"
+NC_URL="https://cloud.dpsg-radolfzell.de/remote.php/dav/public-calendars/"
 
 DIR=$(dirname "$0")
 echo "Downloading calendars to $DIR"
@@ -10,12 +10,12 @@ get_calendar() {
 	name=$2
 	echo "Downloading calendar $name"
 	curl -s -o "$DIR/$name.ics" "$NC_URL$id/?export"
-	sed -i 's/(Andreas)/(DPSG Stamm Impeesa)/' $DIR/$name.ics
+	sed -i 's/(Admin)/(DPSG Stamm Impeesa)/' $DIR/$name.ics
 }
 
-get_calendar BmnSo2YZeZH7BK8m stamm
-get_calendar sGbQKez8aCqTQHLT rover
-get_calendar 4wobNH9tjkCiZmZn pfadis
-get_calendar 6QGzjM8nq2oEAM67 jufis
-get_calendar WzwTkAFxM6Hs3aDw wös
-get_calendar xGPXppXZjESFL7NJ leiter
+get_calendar c5yMSBoWp9qRstkd stamm
+get_calendar KkddcdLgTtQWoJx7 rover
+get_calendar cBe3NCsN4PfGDHNw pfadis
+get_calendar M4b3JGqt6jWGnMKe jufis
+get_calendar sMRJ43N6q8KL6ktw wös
+get_calendar qZygR93cKXjeeSfd leiter
