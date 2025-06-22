3a5f196e81e4ffe6a3b70206989cc5fe137c1170
emhl <emhl@steinr.eu>
2025-06-22 15:10:53 +0200

    remove unused assets

--- assets/events/ical-test.js
+++
@@ -1,7 +0,0 @@
-  // ical feed Termine
-  {
-    url: 'https://cloud.dpsg-bodensee.de/remote.php/dav/public-calendars/2TEopJmk2yQrBWQ9/?export',
-    format: 'ics',
-    color: "pink",     // an option!
-    textColor: "black" // an option!
-  }
